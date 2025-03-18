c1b742c79775b0d143c7e3e9c6c85f26fa915665
Seven260 <f72738h@gmail.com>
2025-03-18 10:53:32 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,8 @@ import json
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 
 # إعدادات البوت
-TOKEN = "7595374455:AAEKtC2OqyjtNnEBJby3KZZAKBhgx-sbG6Q"  # ضع توكن البوت الصحيح هنا
-ADMIN_ID = 7347225275      # ضع معرف الأدمن الصحيح هنا
+TOKEN = "8165570365:AAFxiQdzjETg37Uv0NWqMtcfr0qDT-0vLHY"  # ضع توكن البوت الصحيح هنا
+ADMIN_ID = 734722575      # ضع معرف الأدمن الصحيح هنا
 SUPPORT_LINK = "https://t.me/Vuvuvuuu_bot"  # رابط الدعم لشحن الرصيد
 
 # إعدادات الدعوات
